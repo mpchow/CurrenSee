fb6b93a42986dec823301741ee4a36732247422b
mpchow <mattchow918@gmail.com>
2020-05-27 02:26:37 -0700

    Setup form structure

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import './Input.css'
+
+const currCodes = ['AED', 'ARS', 'AUD', 'BGN', 'BRL', 'BSD', 'CAD', 'CHF', 'CLP', 'CNY', 'COP', 'CZK',
+'DKK', 'DOP', 'EGP', 'EUR', 'FJD', 'GBP', 'GTQ', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'ISK', 'JPY',
+'KRW', 'KZT', 'MXN', 'MYR', 'NOK', 'NZD', 'PAB', 'PEN', 'PHP', 'PKR', 'PLN', 'PYG', 'RON', 'RUB', 'SAR',
+'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'UAH', 'USD', 'UYU', 'ZAR'];
 
 class Input extends React.Component {
    render() {
-      return <div></div>;
+      return (
+      <div className="Input">
+         <form>
+            <select id="currFrom" name="currFrom">
+               <option value={null}>Currency From</option>
+               {currCodes.map( code => {return <option value={code}>{code}</option>})}
+            </select>
+            <select id="currTo" name="currTo">
+               <option value={null}>Currency To</option>
+               {currCodes.map( code => {return <option value={code}>{code}</option>})}
+            </select>
+            <input type="number" id="amount" name="amount" placeholder="Amount"></input>
+            <input type="submit"></input>
+         </form>
+      </div>);
    }
 }
 
