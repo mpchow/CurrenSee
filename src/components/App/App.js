1eff2442a7dc607c8a5b3cc8dffe2e4116336649
mpchow <mattchow918@gmail.com>
2020-05-27 02:47:00 -0700

    Properly interpret data from form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,41 @@ import './App.css';
 import Graph from '../Graph/Graph';
 import Input from '../Input/Input';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h1 className="App-title">Currency Converter</h1>
-      </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-      <div className="App-body">
-        <Input />
-        <Graph />
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const data = {
+      currFrom: formData.get('currFrom'),
+      currTo: formData.get('currTo'),
+      amount: formData.get('amount')
+    };
+
+    console.log(data);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1 className="App-title">Currency Converter</h1>
+        </div>
+
+        <div className="App-body">
+          <Input onSubmit={this.handleSubmit}/>
+          <Graph />
+        </div>
+
       </div>
-      
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
