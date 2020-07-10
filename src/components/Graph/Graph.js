74d781b9c082951393d1cba3e69edb71a212291a
mpchow <mattchow918@gmail.com>
2020-07-09 23:51:10 -0600

    Added test for implementing Chart.js

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Chart from 'chart.js';
+//Use chart.js here
 
 class Graph extends React.Component {
+   chartRef = React.createRef();
+
+
+   componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+   }
+
+
    render() {
-      return <div></div>;
+      return (
+      <div>
+         <canvas id="myChart" ref={this.chartRef}/>
+      </div>);
    }
 }
 
