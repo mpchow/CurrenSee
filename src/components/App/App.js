660a4c3fe200075ec7f5db8733a3ef0552a69265
mpchow <mattchow918@gmail.com>
2020-07-11 00:19:31 -0600

    properly setting state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Graph from '../Graph/Graph';
 import Input from '../Input/Input';
+import exchange from '../../Util/ExchangeRate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
@@ -19,21 +21,32 @@ class App extends React.Component {
       amount: formData.get('amount')
     };
 
-    console.log(data);
+    console.log(typeof data.currFrom);
+
+    exchange.search(data.currFrom, data.currTo)
+    .then(rate => this.setState({rate: rate, data: data}));
   }
 
 
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
           <h1 className="App-title">Currency Converter</h1>
+          <h1>{this.state.rate}</h1>
         </div>
 
         <div className="App-body">
           <Input onSubmit={this.handleSubmit}/>
-          <Graph />
+          {this.state.data ? <p>{this.state.data.amount}</p> : null}
+          <div className="Graphs">
+            <Graph />
+
+          </div>
+
         </div>
+
 
       </div>
     );
