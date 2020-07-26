b1985cb0363c02ec4c09a3e7f65bddcd94997bb3
mpchow <mattchow918@gmail.com>
2020-07-25 23:52:43 -0600

    Created graph component

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,38 +1,58 @@
 import React from 'react';
 import Chart from 'chart.js';
+import './Graph.css';
 //Use chart.js here
+let myLineChart;
 
-class Graph extends React.Component {
+class Graph extends React.PureComponent {
    chartRef = React.createRef();
 
 
+   chartRef = React.createRef();
+
    componentDidMount() {
-        const myChartRef = this.chartRef.current.getContext("2d");
-        
-        new Chart(myChartRef, {
-            type: "line",
-            data: {
-                //Bring in data
-                labels: ["Jan", "Feb", "March"],
-                datasets: [
-                    {
-                        label: "Sales",
-                        data: [86, 67, 91],
-                    }
-                ]
-            },
-            options: {
-                //Customize chart options
-            }
-        });
+       this.buildChart();
    }
 
+   componentDidUpdate() {
+       this.buildChart();
+   }
 
-   render() {
-      return (
-      <div>
-         <canvas id="myChart" ref={this.chartRef}/>
-      </div>);
+   buildChart = () => {
+       const myChartRef = this.chartRef.current.getContext("2d");
+       const data = this.props.data.dates.map(item => { return {rate: item.rate, date: new Date(item.date)};}).sort((a, b) => a.date - b.date);
+       const dates = data.map(date => `${date.date.getMonth()}-${date.date.getDate()}-${date.date.getYear()+1900}` );
+       const rates = data.map(date => date.rate);
+
+       myLineChart = new Chart(myChartRef, {
+           type: "line",
+           data: {
+               //Bring in data
+               labels: dates,
+               datasets: [
+                   {
+                       label: "Rate",
+                       data: rates,
+                   }
+               ]
+           },
+           options: {
+               title: {
+                   display: true,
+                   text: `${this.props.data.input.currTo} in relation to ${this.props.data.input.currFrom}`
+               }
+
+           }
+       });
+    }
+
+
+    render() {
+       return (
+        <div className="Graph">
+           <button onClick={() => this.props.delete(this.props.data.id)}>Delete Graph</button>
+           <canvas id="myChart" ref={this.chartRef}/>
+        </div>);
    }
 }
 
